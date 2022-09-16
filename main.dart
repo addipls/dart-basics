298dcd9897ee6d189524282495ba2cb299ef5438
addison <72429964+addipls@users.noreply.github.com>
2022-09-16 03:03:29 +0000

    String concatenation

--- main.dart
+++ main.dart
@@ -2,13 +2,32 @@ void main() {
   print("Dart Basics");
   print("---------------");
   loops();
+  print(asciiCat());
 }
 
 /*
- * Print 0 - 9 with loops
+ * Print 0 - 9 with loops, also adding values into a string
  */
 void loops() {
   for (var i = 0; i < 10; i++) {
     print("Value ${i}");
   }
 }
+
+/*
+ _._     _,-'""`-._
+(,-.`._,'(       |\`-/|
+    `-.-' \ )-`( , o o)
+          `-    \`_`"'-
+ */
+String asciiCat() {
+  String cat = " _._     _,-'" "`-._\n";
+  cat += "(,-.`._,'(       |\`-/|\n";
+  cat += "    `-.-' \ )-`( , o o)\n";
+  cat += "          `-    \`_`''-\n";
+  return cat;
+}
+
+/*
+ * While loop
+ */
