e35b5b33221a3b995360456c0c54e377772484cf
addison <72429964+addipls@users.noreply.github.com>
2022-09-16 02:39:06 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Dart Basics");
+  print("---------------");
+  loops();
+}
+
+/*
+ * Print 0 - 9 with loops
+ */
+void loops() {
+  for (var i = 0; i < 10; i++) {
+    print("Value ${i}");
+  }
+}
